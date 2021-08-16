c49fa860dca7c9a5d8c2e7fa9375839059c6189e
Filip Petrovic <filippetrovic603@gmail.com>
2021-08-16 10:46:41 +0200

    Expanded test framework for all tests, starting to integrate VGA test buffer

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@
 #![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
@@ -22,6 +23,7 @@ pub extern "C" fn _start() {
 }
 
 // Function called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
@@ -30,20 +32,68 @@ fn panic(info: &PanicInfo) -> ! {
 
 // Testing
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("[Running {} tests]", tests.len());
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("\n[Running {} tests]", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
 }
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("[{}] ... ", core::any::type_name::<T>());
+        self();
+        serial_println!("[PASS]");
+    }
+}
+
+// Test cases - MOVE TO VGA BUFFER MODULE
 #[test_case]
 fn trivial_assertion() {
-    print!("[Test assertion] ... ");
-    assert_eq!(1, 1);
-    println!("[PASS]");
+    assert_eq!(1, 1);    
+}
+
+#[test_case]
+fn test_println_simple() {
+    println!("[Simple printing test - output]")
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("[Stress printing test - output]")
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "[Example test string that fits on a single line]";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
+
+
+// Panic test
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[FAIL]\n");
+    serial_println!("[ERROR] ... [{}]\n", info);
+    exit_qemu(QemuExitCode::Failed);
+
+    loop{}
 }
 
 // Exit
